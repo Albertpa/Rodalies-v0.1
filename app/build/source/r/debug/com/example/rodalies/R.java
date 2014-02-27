d8edb9b1b5cff70ec85105f721d739c5ed33e514
Jesús Escribano Gómez <jescribano@grupospec.com>
2014-02-27 12:42:38 +0100

    Logo
    - Added: Añadido logo de la app.
    - Added. Añadido archivo gitignore. La información se ha extraido de
    aqui: http://goo.gl/xJ6sds

--- app/build/source/r/debug/com/example/rodalies/R.java
+++ app/build/source/r/debug/com/example/rodalies/R.java
@@ -857,6 +857,7 @@ containing a value of this type.
         public static final int bird_blue_48=0x7f020057;
         public static final int bird_gray_48=0x7f020058;
         public static final int ic_launcher=0x7f020059;
+        public static final int logo=0x7f02005a;
     }
     public static final class id {
         public static final int Spinner_Lineas1=0x7f080043;

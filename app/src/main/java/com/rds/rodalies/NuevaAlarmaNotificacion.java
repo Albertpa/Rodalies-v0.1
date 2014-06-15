6fd26097ee49cd8107534e3c531590e58412e90c
Albert <Albertpa@gmail.com>
2014-06-15 19:09:30 +0200

    Notificaciones
    - Primeros pasos de la Vista de configuración de notificaciones.

--- app/src/main/java/com/rds/rodalies/NuevaAlarmaNotificacion.java
+++ app/src/main/java/com/rds/rodalies/NuevaAlarmaNotificacion.java
@@ -3,43 +3,54 @@ package com.rds.rodalies;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 
 public class NuevaAlarmaNotificacion extends FragmentActivity {
+
+
+    private TimePicker timePicker1;
+
+    private int hour;
+    private int minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nueva_alarma_notificacion);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nueva_alarma_notificacion, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new ConfiguradorNotificaciones();
         newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    // display current time
+    public void setCurrentTimeOnView() {
+
+        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
+
+        final Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+
+
+        //si no hay datos guardados asignamos la hora actual a los pickers
+        // set current time into timepicker
+        timePicker1.setCurrentHour(hour);
+        timePicker1.setCurrentMinute(minute);
+
+    }
+
+    public void guardar(View v){
+        Log.e("Rodalies", "guarda elemento");
+        finish();
     }
 
 }
